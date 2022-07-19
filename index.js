d953f6c2656bdaa8f1d46d36c4f5d33e595e01e9
Zoe Mendez <zoemendez316@gmail.com>
2022-07-19 17:00:27 -0600

    Done with assignment

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
-
 function addingEventListener() {
     const input = document.getElementById('button');
     input.addEventListener('click', function () {
         alert('I was clicked!')
     })
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function addingEventListener() {
+//     const input = document.getElementById('button');
+//     input.addEventListener('click', function () {
+//         alert('I was clicked!')
+//     })
+// }
 
 
